5e00d16eec43e1dcdf1369f141eefcc8b1e74369
YigithanSonmez <yigithan.sonmez@mildmania.com>
2023-01-17 21:31:34 +0300

    Initial Commit

---
+++ PomodoroMenuBar/PomodoroMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroMenuBarApp.swift
+//  PomodoroMenuBar
+//
+//  Created by Yiğithan Sönmez on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
